82e451fecb84349c5b4bf6a23c3e211eaac82bb7
willyanfx <willyan.fx@gmail.com>
2022-11-11 17:11:20 -0800

    create controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from "express";
-import { StoreController } from "./controllers/storeController";
+import { StoreController } from "./controllers/StoreController";
 
 const router = Router();
 
 const storeController = new StoreController();
 
 router.get("/", storeController.handle);
+router.get("/add", storeController.add);
+router.post("/create", storeController.create);
 
 router.get("/reverse/:name", (req: Request, res: Response) => {
   let reverse = [...req.params.name].reverse().join("");
